6892bcfb1b484c1da23d38e45bd039590f693057
Kurnia D Win <kurnia.d.win@gmail.com>
2020-05-17 22:04:54 +0700

    update

--- errhandlerext/errhandlerext.go
+++ errhandlerext/errhandlerext.go
@@ -9,6 +9,6 @@ import (
 
 // LogAndExit is handler for https://godoc.org/github.com/payfazz/go-errors/errhandler#With
 func LogAndExit(err error) {
-	errors.PrintTo(stdlog.Err, err)
+	errors.PrintTo(stdlog.Err(), err)
 	os.Exit(1)
 }
